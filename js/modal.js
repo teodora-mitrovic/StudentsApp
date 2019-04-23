997404dae54e05ae150cb002e71ae00b4f975202
root <root@fedora28.localdomain>
2019-04-23 16:00:35 +0200

    Initial stucture

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $('#update-btn').click(function(){
+        
+        //change modal title
+        
+        //show search input and button
+        
+        
+        //disable other inputs until the student is returned
+
+    })
+
+
+})
